c4daff72152e586734f8d394aae505d865beae38
AnshulRaghav <anshulraghav2222@gmail.com>
2021-10-09 15:43:10 +0530

    added common styles

---
+++ src/commonStyle.js
@@ -0,0 +1,40 @@
+export const colors = {
+    primary      : '#0071DF',
+    secondary    : '#FEFEFE',
+    success      : '#2FAE60',
+    alert        : '#EB5757',
+    textPrimary  : '#000000',
+    textLight    : '#717171',
+    textFaint    : '#DADADA',
+    textXLight   : '#F2F2F2',
+    primaryLight : '#D6EBFF'
+}
+
+export const fontSize = {
+    title : 24,
+    h1    : 16,
+    h2    : 15,
+    h3    : 14,
+    h4    : 13,
+    h5    : 12,
+    h6    : 11,
+    h7    : 10,
+}
+
+export const fontFamily = {
+    primary        : 'Source Sans pro',
+    primaryBold    : 'Source Sans pro SemiBold',
+    primaryRegular : 'Source Sans pro Regular',
+    secondaryBold  : 'Gilroy SemiBold',
+    secondaryMd    : 'Gilroy Medium',
+    tertiary       : 'Poppins',
+}
+
+// to be used with paddings and margins
+export const levels = {
+    l1 : 4,
+    l2 : 8,
+    l3 : 12,
+    l4 : 16,
+    l5 : 24,
+}
